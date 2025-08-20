a2cd1b32ffbb87ee0acd01fd8e1fe4cc55bfc0b3
JENIUSTECHS <junaidehsan0786@gmail.com>
2025-08-20 11:36:56 +0500

    Update flowers.js

--- flowers.js
+++ flowers.js
@@ -3,7 +3,7 @@ onload = () => {
     const c = setTimeout(() => {
       document.body.classList.remove("not-loaded");
   
-      const titles = ('Happy Birthday My Dearest #lipstick. Thanks for Everything.  ').split('')
+      const titles = ('Happy Birthday My Dearest #lipstick. Stay Blessed.  ').split('')
       const titleElement = document.getElementById('title');
       let index = 0;
   
